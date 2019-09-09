331c87d50011325b7b13bb416a771b67ec3c9db3
Vitali Kaspler <vitali.kaspler@gmail.com>
2019-09-09 18:46:23 +0300

    Module exports

--- tezos/.js
+++ tezos/.js
@@ -1,14 +1,19 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function runCommand(){
+module.exports = {
+  createStandart: async ()=>{
         const { stdout, stderr } = await exec(`cd ~/tezos-nft-sample
 python -m client originatestandard
 sudo npm run bake`);
         console.log('stdout:', stdout);
         console.log('stderr:', stderr);
+  },
+  createWithBalance: async ()=>{
+        const { stdout, stderr } = await exec(`cd ~/tezos-nft-sample
+python -m client originatebalance
+sudo npm run bake`);
+        console.log('stdout:', stdout);
+        console.log('stderr:', stderr);
+  }
 }
-
-(async ()=>{
-        await runCommand();
-})();
